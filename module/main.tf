72c99984a5c7b8ab311c562c71a306e2ee539841
Amit Bisht <amit.bisht@dishtv.in>
2025-08-13 19:21:57 +0530

    ll

--- module/main.tf
+++ module/main.tf
@@ -1,23 +1,5 @@
-module "kuku" {
+module "tinku" {
 source = "../infra"
-res_grp = "inni"
-res_loc = "CentralIndia"
-}
-
-module "pokki" {
-source = "../infra"
-res_grp = "mon"
-res_loc = "CentralIndia"
-}
-
-module "fifo" {
-source = "../infra"
-res_grp = "rishu"
-res_loc = "CentralIndia"
-}
-
-module "limo" {
-source = "../infra"
-res_grp = "lion"
+res_grp = "jiya"
 res_loc = "CentralIndia"
 }
